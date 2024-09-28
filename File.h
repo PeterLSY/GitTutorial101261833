81a14cc2249f7bcc96b16b6a0d19edc977e79134
PeterLSY <89462460+PeterLSY@users.noreply.github.com>
2024-09-28 18:49:21 -0400

    Update File.h

--- File.h
+++ File.h
@@ -1,33 +1,25 @@
 #ifndef FILE_H
 #define FILE_H
 
-#include <iostream>
 #include <string>
 #include "Date.h"
 
-using namespace std;
-
 class File {
 public:
-    // Constructors
-    File(const string& name, const string& content, Date& date);
+    // Constructor
+    File(std::string name, std::string content, Date& date);
 
-    // Getters
-    string getName() const;
-    string getContent() const;
-    Date getDate() const;
-
-    // Comparison function
-    bool lessThan(const File& otherFile) const;
-
-    // Print functions
-    void print() const;           // Print file metadata (name and date)
-    void printContents() const;   // Print file metadata and content
+    // Functions
+    bool lessThan(File& other);
+    void print();
+    void printContents();
 
 private:
-    string name;
-    string content;
-    Date lastModified;
+    // Member variables
+    std::string name;
+    std::string content;
+    Date date;  // Indicates when the File was last modified
 };
 
 #endif
+
